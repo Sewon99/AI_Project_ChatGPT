f9f3f680ece4588417d76c2df7971cf4be62677a
Sewon99 <qwrtyuio99@naver.com>
2023-06-02 20:50:36 +0900

    first

--- lib/constants/api_consts.dart
+++ lib/constants/api_consts.dart
@@ -1,2 +1,2 @@
 String BASE_URL = "https://api.openai.com/v1";
-String API_KEY = "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";
+String API_KEY = "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";
