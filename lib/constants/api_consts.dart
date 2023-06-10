94d4b962e9d92460b518cb4bb4f8bd37151e95f2
kimpeter2018 <kimpeter2018@gmail.com>
2023-06-10 17:19:39 +0900

    API key update

--- lib/constants/api_consts.dart
+++ lib/constants/api_consts.dart
@@ -1,2 +1,2 @@
 String BASE_URL = "https://api.openai.com/v1";
-String API_KEY = "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";
+String API_KEY = "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";
--- lib/services/assets_manager.dart
+++ lib/services/assets_manager.dart
@@ -1,6 +1,6 @@
 class AssetsManager {
   static String imagePath = "assets/images";
   static String userImage = "$imagePath/person.png";
-  static String botImage = "$imagePath/chat_logo.png";
+  static String botImage = "$imagePath/disko_icon.png";
   static String openaiLogo = "$imagePath/openai_logo.jpg";
 }
