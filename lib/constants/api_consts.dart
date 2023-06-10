21e15e093bb5196e295ce77b1c3df6b05576f3f4
kimpeter2018 <kimpeter2018@gmail.com>
2023-06-10 18:06:23 +0900

    API Key 수정

--- lib/constants/api_consts.dart
+++ lib/constants/api_consts.dart
@@ -1,2 +1,2 @@
 String BASE_URL = "https://api.openai.com/v1";
-String API_KEY = "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";
+String API_KEY = "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";
